9ffc1c147032701f275270ef585fe80e2fa42f5b
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-02-02 00:05:27 +0800

    feat: creates files on button click and running files on button click

--- app/index.tsx
+++ app/index.tsx
@@ -5,12 +5,38 @@ import { ThemeToggle } from '~/components/ToggleTheme';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { Text } from '~/components/ui/text';
+import * as FileSystem from 'expo-file-system';
 
 export default function Home() {
   const [value, setValue] = useState('');
 
-  function createPlugin(name: string) {
-    alert(name);
+  async function createPlugin(name: string) {
+    const plugins_dir = FileSystem.documentDirectory + 'plugins';
+    const plugin_file = plugins_dir + `/${name}`;
+    if (!(await FileSystem.getInfoAsync(plugins_dir)).exists) {
+      await FileSystem.makeDirectoryAsync(plugins_dir);
+    }
+
+    await FileSystem.writeAsStringAsync(
+      plugin_file,
+      `export default function run(){ alert('hello from ${name}') }`
+    );
+  }
+
+  async function runPlugins() {
+    const plugins_dir = FileSystem.documentDirectory + 'plugins';
+    for (const plugin of await FileSystem.readDirectoryAsync(plugins_dir)) {
+      const code = await FileSystem.readAsStringAsync(plugins_dir + `/${plugin}`);
+      const func = Function(
+        'require',
+        'module',
+        `const exports = module.exports = {}; 
+      ${code}; 
+      return exports.default`
+      );
+
+      alert(func.toString());
+    }
   }
 
   return (
@@ -37,8 +63,16 @@ export default function Home() {
             </Button>
           </View>
           <View className="flex-row gap-2">
-            <Button className="w-1/2" onPress={() => alert('run')} variant={'default'}>
+            <Button className="w-1/2" onPress={() => runPlugins()} variant={'default'}>
               <Text>Run Plugins</Text>
+            </Button>
+          </View>
+          <View className="flex-row gap-2">
+            <Button
+              className="w-1/2"
+              onPress={() => alert('deleteplugins')}
+              variant={'destructive'}>
+              <Text>Delete All Plugins</Text>
             </Button>
           </View>
         </SafeAreaView>
