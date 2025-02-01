81983fa7ac5e78610bf2e88f4938289c62f5813b
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-02-02 01:16:29 +0800

    feat: updates example plugin code

--- plugins/example.plugin.js
+++ plugins/example.plugin.js
@@ -1,4 +1,3 @@
-const toast = require('react-native-toast-message');
 module.exports = function run(name) {
-  toast.show({ type: 'success', text1: 'hello from ' + name });
+  return 'hello from ' + name;
 };
