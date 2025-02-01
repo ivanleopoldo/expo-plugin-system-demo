6c28e22992c7202f70f846efaf27273dd25f7b93
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-02-01 23:38:01 +0800

    UI setup

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,17 @@
-import { Link, Stack } from "expo-router";
+import { Link, Stack } from 'expo-router';
 
-import { Text } from "react-native";
-
-import { Container } from "~/components/Container";
+import { Text, View } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Oops!" }} />
-      <Container>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <View>
         <Text className={styles.title}>This screen doesn't exist.</Text>
         <Link href="/" className={styles.link}>
           <Text className={styles.linkText}>Go to home screen!</Text>
         </Link>
-      </Container>
+      </View>
     </>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,48 @@
-import { Stack, Link } from "expo-router";
-
-import { Button } from "~/components/Button";
-import { Container } from "~/components/Container";
-import { ScreenContent } from "~/components/ScreenContent";
+import { Stack } from 'expo-router';
+import { useState } from 'react';
+import { Keyboard, SafeAreaView, TouchableWithoutFeedback, View } from 'react-native';
+import { ThemeToggle } from '~/components/ToggleTheme';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { Text } from '~/components/ui/text';
 
 export default function Home() {
+  const [value, setValue] = useState('');
+
+  function createPlugin(name: string) {
+    alert(name);
+  }
+
   return (
     <>
-      <Stack.Screen options={{ title: "Home" }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: "/details", params: { name: "Dan" } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+      <Stack.Screen options={{ title: 'Home', headerRight: () => <ThemeToggle /> }} />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SafeAreaView className="min-h-full w-full items-center justify-center gap-4">
+          <View className="flex-row gap-2">
+            <Input
+              placeholder="Plugin Name"
+              onChangeText={(text) => setValue(text)}
+              className="w-1/2"
+              value={value}
+            />
+            <Button
+              onPress={(_) => {
+                if (value !== '') {
+                  createPlugin(value);
+                  setValue('');
+                }
+              }}
+              variant={'secondary'}>
+              <Text>+</Text>
+            </Button>
+          </View>
+          <View className="flex-row gap-2">
+            <Button className="w-1/2" onPress={() => alert('run')} variant={'default'}>
+              <Text>Run Plugins</Text>
+            </Button>
+          </View>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
     </>
   );
 }
